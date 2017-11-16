52c24a2ebc11d069caf1aafeacc9dc1e79df7276
Robert Smithers <rsmithers@hs-242-imac-06.princetonk12.org>
2017-11-16 09:27:10 -0500

    Appended VendingMachine.java

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -3,7 +3,10 @@
  */
 
 /**
- * @author rsmithers
+ * @author Robert Smithers and Devin Roychowdhury
+ * @date 11/16/17
+ * @description Creates a detailed collection of information and statistics on a vending
+ * machine using the snacks built into the code and the purchases
  *
  */
 public class VendingMachine {
@@ -11,9 +14,15 @@ public class VendingMachine {
 	/**
 	 * @param args
 	 */
+	
+	public VendingMachine()
+	{
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
+	
 }
